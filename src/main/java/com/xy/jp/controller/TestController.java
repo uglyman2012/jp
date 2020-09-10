84ebfc9729935f1253c303ca95839a2e32585780
yang <123>
2020-09-10 16:31:44 +0800

    kt

--- src/main/java/com/xy/jp/controller/TestController.java
+++ src/main/java/com/xy/jp/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.xy.jp.controller;
 
+import com.xy.jp.bean.Student;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * <p>
@@ -16,6 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(value = "/test")
     public String test(){
-        return "sucess_hello";
+        return "sucess_hello_kt";
     }
 }
