52c5b22b3024ee678da62ddd44d06c1bb74231e2
yang <123>
2020-11-05 14:00:00 +0800

    2222

--- src/main/java/com/xy/jp/controller/TestController.java
+++ src/main/java/com/xy/jp/controller/TestController.java
@@ -22,7 +22,7 @@ public class TestController {
     private RabbitTemplate rabbitTemplate;
     @RequestMapping(value = "/test")
     public String test(){
-        return "sucess_hello";
+        return "sucess_hello222";
     }
 
     //@RequestMapping(value = "/test2")
